a42b1270f1767b6bc519ea59e2a985620003d64b
dee-d-dev <adedigbaadedotunemmanuel@gmail.com>
2021-06-12 03:24:51 +0100

    changed Home css file to css modules

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/home.css";
+import HomeCSS from "../styles/home.module.css";
 import { useState } from "react";
 import {Link} from 'react-router-dom'
 
@@ -10,27 +10,27 @@ const Home = () => {
   const [like, setLike] = useState(0);
   return (
     <div>
-      <div className="header">
-        <div className="brand">
+      <div className={HomeCSS.header}>
+        <div className={HomeCSS.brand}>
           <img src="/logo.png" width="100px" alt="" />
         </div>
 
-        <div className="search-section">
+        <div className={HomeCSS.search_section}>
           <div>
             <input type="text" />
           </div>
-          <div className="search-btn">
+          <div className={HomeCSS.search_btn}>
             <button>Search</button>
           </div>
         </div>
 
-        <div className="cart-icon">
+        <div className={HomeCSS.cart_icon}>
           <img src="/cart icon.png" alt="" />
-          <div className="item-count">{count}</div>
+          <div className={HomeCSS.item_count}>{count}</div>
         </div>
       </div>
 
-      <div className="navigation">
+      <div className={HomeCSS.navigation}>
         <ul>
           <li>
             <a href="">Home</a>
@@ -53,24 +53,24 @@ const Home = () => {
           <li>
             <a href="">Login</a>
           </li>
-          <li className="create-acc">
+          <li className={HomeCSS.create_acc}>
             <Link to="/sign-in">Create a Free Account</Link>
           </li>
-          <li className="love">
+          <li className={HomeCSS.love}>
             <img src="/love.png" alt="" onClick={(e) => setLike(like + 1)} />
-            <div className="love-count">{like}</div>
+            <div className={HomeCSS.love_count}>{like}</div>
           </li>
         </ul>
       </div>
 
-      <div className="section-1">
-        <div className="display-board">
-          <div className="d-b-top">
+      <div className={HomeCSS.section_1}>
+        <div className={HomeCSS.display_board}>
+          <div className={HomeCSS.d_b_top}>
             <h1>
               Fresh <br /> <span>Vegetables</span>
             </h1>
 
-            <div className="memo">
+            <div className={HomeCSS.memo}>
               <p>
                 "Nevertheless, one should accept that the possibility of
                 achieving core concept of the existing network, as far as the
@@ -80,10 +80,10 @@ const Home = () => {
               </p>
             </div>
 
-            <button className="shop-btn">SHOP NOW</button>
+            <button className={HomeCSS.shop_btn}>SHOP NOW</button>
           </div>
 
-          <div className="item-show">
+          <div className={HomeCSS.item_show}>
             <ul>
               <li>
                 <img src="/banana.png" alt="banana" width="80px" />
@@ -106,50 +106,50 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="section-2">
-        <div className="location">
+      <div className={HomeCSS.section_2}>
+        <div className={HomeCSS.location}>
           <p>Lagos</p>
           <img src="/Group.png" width="50px" alt="" />
         </div>
 
-        <div className="item-cards">
-          <div className="item-card vegetables-card">
-            <div className="item-1">
-              <div className="item-text">
-                <p className="item-name vegetable">VEGETABLE</p>
-                <p className="item-type">GREEN WORLD</p>
+        <div className={HomeCSS.item_cards}>
+          <div className={HomeCSS.vegetables_card}>
+            <div className={HomeCSS.item_1}>
+              <div className={HomeCSS.item_text}>
+                <p className={HomeCSS.vegetable}>VEGETABLE</p>
+                <p className={HomeCSS.item_type}>GREEN WORLD</p>
               </div>
-              <div className="promo">Get 40% OFF selected items</div>
+              <div className={HomeCSS.promo}>Get 40% OFF selected items</div>
 
-              <button className="learn-btn vegetable-btn">LEARN MORE ›</button>
+              <button className={HomeCSS.vegetable_btn}>LEARN MORE ›</button>
             </div>
           </div>
-          <div className="item-card fresh-fruits-card">
-            <div className="item-2">
-              <div className="item-text">
-                <p className="item-name fresh-fruits">FRESH FRUITS</p>
-                <p className="item-type">HEALTHY FOODS</p>
+          <div className={HomeCSS.fresh_fruits_card}>
+            <div className={HomeCSS.item_2}>
+              <div className={HomeCSS.item_text}>
+                <p className={HomeCSS.fresh_fruits}>FRESH FRUITS</p>
+                <p className={HomeCSS.item_type}>HEALTHY FOODS</p>
               </div>
-              <div className="promo">Get 80% OFF selected items</div>
+              <div className={HomeCSS.promo}>Get 80% OFF selected items</div>
 
-              <button className="learn-btn fresh-fruits-btn">
+              <button className={HomeCSS.fresh_fruits_btn}>
                 LEARN MORE ›
               </button>
             </div>
           </div>
         </div>
 
-        <div className="best-sell">
+        <div className={HomeCSS.best_sell}>
           <h1>BEST SELLER</h1>
 
-          <div className="best-items">
+          <div className={HomeCSS.best_items}>
             {items.map((item) => {
               return (
-                <div className="item">
-                  <div className="item-img"></div>
-                  <div className="item-name">Fresh Water Melon</div>
-                  <div className="item-stars">stars</div>
-                  <div className="item-price">₦5000</div>
+                <div className={HomeCSS.item}>
+                  <div className={HomeCSS.item_img}></div>
+                  <div className={HomeCSS.item_name}>Fresh Water Melon</div>
+                  <div className={HomeCSS.item_stars}>stars</div>
+                  <div className={HomeCSS.item_price}>₦5000</div>
                 </div>
               );
             })}
@@ -157,26 +157,26 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="section-3">
-        <div className="s-3-top">
-          <p className="call">HURRY UP!</p>
+      <div className={HomeCSS.section_3}>
+        <div className={HomeCSS.s_3_top}>
+          <p className={HomeCSS.call}>HURRY UP!</p>
           <h1>DEALS OF THE WEEK</h1>
-          <p className="text">
+          <p className={HomeCSS.text}>
             "It's a well known fact that the lack of knowledge of cost of the
             competitive development and manufacturing requires urgent actions to
             be taken towards the technology of interrelational regulatinns"
           </p>
         </div>
 
-        <div className="deals">
-          <div className="deals-item">
+        <div className={HomeCSS.deals}>
+          <div className={HomeCSS.deals_item}>
             {items.map((item) => {
               return (
-                <div className="d-item">
-                  <div className="d-item-img"></div>
-                  <div className="d-item-name">Fresh Fish</div>
-                  <div className="d-item-stars">stars</div>
-                  <div className="d-item-price">₦2000</div>
+                <div className={HomeCSS.d_item}>
+                  <div className={HomeCSS.d_item_img}></div>
+                  <div className={HomeCSS.d_item_name}>Fresh Fish</div>
+                  <div className={HomeCSS.d_item_stars}>stars</div>
+                  <div className={HomeCSS.d_item_price}>₦2000</div>
                 </div>
               );
             })}
@@ -202,38 +202,38 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="services">
-          <div className="service">
-            <div className="service-icon">
+        <div className={HomeCSS.services}>
+          <div className={HomeCSS.service}>
+            <div className={HomeCSS.service_icon}>
               <img src="/delivery-truck.png" width="30px" alt="" />
             </div>
-            <div className="service-text">
-              <h4 className="service-head">Free & Next Day Delivery</h4>
-              <p className="service-info">
+            <div className={HomeCSS.service_text}>
+              <h4 className={HomeCSS.service_head}>Free & Next Day Delivery</h4>
+              <p className={HomeCSS.service_info}>
                 The Organization of User-Friendly Analysis
               </p>
             </div>
           </div>
 
-          <div className="service">
-            <div className="service-icon">
+          <div className={HomeCSS.service}>
+            <div className={HomeCSS.service_icon}>
               <img src="/rating.png" width="27px" alt="" />
             </div>
-            <div className="service-text">
-              <h4 className="service-head">100% Satisfaction Gurantee</h4>
-              <p className="service-info">
+            <div className={HomeCSS.service_text}>
+              <h4 className={HomeCSS.service_head}>100% Satisfaction Gurantee</h4>
+              <p className={HomeCSS.service_info}>
                 The Organization of User-Friendly Analysis
               </p>
             </div>
           </div>
 
-          <div className="service">
-            <div className="service-icon">
+          <div className={HomeCSS.service}>
+            <div className={HomeCSS.service_icon}>
               <img src="/discount.png" width="25px" alt="" />
             </div>
-            <div className="service-text">
-              <h4 className="service-head">The Great Daily Deals Discount</h4>
-              <p className="service-info">
+            <div className={HomeCSS.service_text}>
+              <h4 className={HomeCSS.service_head}>The Great Daily Deals Discount</h4>
+              <p className={HomeCSS.service_info}>
                 The Organization of User-Friendly Analysis
               </p>
             </div>
@@ -241,38 +241,38 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="section-4">
+      <div className={HomeCSS.section_4}>
         <div>
-          <ul className="company-infos">
-            <li className="details">
+          <ul className={HomeCSS.company_infos}>
+            <li className={HomeCSS.details}>
               <div>
                 <img
                   src="/footer logo.png"
                   width="100px"
-                  className="brand"
+                  className={HomeCSS.brand}
                   alt="footer-logo"
                 />
               </div>
 
-              <ul className="company-contact">
-                <li className="phone detail">
-                  <div className="detail_icon">
+              <ul className={HomeCSS.company_contact}>
+                <li className={HomeCSS.detail}>
+                  <div className={HomeCSS.detail_icon}>
                     <img src="/call_icon.png" alt="" />
                   </div>
                   <div>
                     <p>+234 812 2333 333</p>
                   </div>
                 </li>
-                <li className="mail detail">
-                  <div className="detail_icon">
+                <li className={HomeCSS.detail}>
+                  <div className={HomeCSS.detail_icon}>
                     <img src="/mail_icon.png" alt="" />
                   </div>
                   <div>
                     <p>contact@themarketplace.com</p>
                   </div>
                 </li>
-                <li className="website detail">
-                  <div className="detail_icon">
+                <li className={HomeCSS.detail}>
+                  <div className={HomeCSS.detail_icon}>
                     <img src="/website.png" alt="" />
                   </div>
                   <div>
@@ -281,7 +281,7 @@ const Home = () => {
                 </li>
               </ul>
             </li>
-            <li className="locations">
+            <li className={HomeCSS.locations}>
               <h3>TOP CITIES</h3>
 
               <ul>
@@ -316,15 +316,15 @@ const Home = () => {
           </ul>
         </div>
 
-        <div className="footer">
-          <div className="copyrights">
+        <div className={HomeCSS.footer}>
+          <div className={HomeCSS.copyrights}>
             ©copyright 2020, Ninja Creative Studio, All rights reserved
           </div>
 
-          <div className="social-media">
-            <div className="facebook">fb icon</div>
-            <div className="instagram">IG icon</div>
-            <div className="twitter">Twitter icon</div>
+          <div className={HomeCSS.social_media}>
+            <div className={HomeCSS.facebook}>fb icon</div>
+            <div className={HomeCSS.instagram}>IG icon</div>
+            <div className={HomeCSS.twitter}>Twitter icon</div>
           </div>
         </div>
       </div>
